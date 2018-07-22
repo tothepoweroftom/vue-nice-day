350812337a57190c4a121f3a640f4e62d3800327
tom <powerth@tcd.ie>
2018-07-22 11:38:11 +0200

    [FIRST] commit. App setup. API tested.

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <div id="content">
+       <router-view/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+}
+
+#content {
+  background-color: #fbfbfb;
+  border: 1px solid rgb(51, 51, 51);
+  display: flex;
+  flex-direction: column;
+  margin-top: 20vh;
+  max-width: 800px;
+  min-height: 510px;
+  padding: 32px;
+  position: relative;
+  width: 100%;
+  z-index: 1;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hello">
+    
+  </div>
+</template>
+
+<script>
+import axios from '../plugins/axios.js'
+
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+    }
+  },
+
+  mounted() {
+    this.getWeatherData();
+  },
+
+  methods: {
+    async getWeatherData() {
+      let apiKey = '9c509a497e575d217c615e84e05679ea'
+
+      let city = 'budapest'; 
+        let response = await axios.get(`?q=${city}&appid=${apiKey}`)
+        console.log(response);
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
